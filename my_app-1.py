68a7d07868e361a6c7b02dd73bcc20902151b274
AlionA5 <kasinskayaalyona333333@gmail.com>
2025-09-21 14:29:03 +0200

    Update my_app-1.py

--- my_app-1.py
+++ my_app-1.py
@@ -1,16 +1,1 @@
-import streamlit as st
-import pandas as pd
-import os
-import base64 
-import numpy as np
-import altair as alt
 
-background: "#617DED"
-st.write("Hi my cute and kind pookie🥹")
-st.image(os.path.join(os.getcwd(),"stickers", "pig.webp"),width=200)
-st.divider()
-st.write('here are 2 buttons that do not work🥳')
-pressed_yes=st.button('Yes')
-print(pressed_yes)
-pressed_no=st.button('No')
-print(pressed_no)
