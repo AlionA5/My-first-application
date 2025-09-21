157d5a877213747101a43eb566589d68136ddd81
AlionA5 <kasinskayaalyona333333@gmail.com>
2025-09-21 14:18:51 +0200

    initial commit

---
+++ my_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.write("Hi my cute and kind pookie🥹")
+st.divider()
+st.write('Wanna play bingo game?')
+pressed_yes=st.button('Yes')
+print(pressed_yes)
+pressed_no=st.button('No')
+print(pressed_no)
